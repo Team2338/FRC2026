9c969c555a4586e17fbfd3c6c8561fb95db7e117
Caleb Herring <calebsethherring+github@gmail.com>
2026-02-16 13:06:08 -0600

    value changes

--- src/main/java/team/gif/robot/OI.java
+++ src/main/java/team/gif/robot/OI.java
@@ -115,7 +115,7 @@ public class OI {
         //dStart.and(dDPadLeft).onTrue(new InstantCommand(Robot.pivotMotor::deployedEncoder).ignoringDisable(true));
         dY.whileTrue(new ShooterRPM());
         dLBump.whileTrue(new IndexerBack(0.25).andThen(new IndexerPercent())); //might change to up later
-        dLBump.onTrue(new WaitCommand(1.0).andThen(new CollectorAutoPivot().withTimeout(1.5)));
+        dLBump.onTrue(new WaitCommand(0.4).andThen(new CollectorAutoPivot().withTimeout(1.3)));
 //        dRBump.whileTrue(new RepeatCommand(new IndexerBack().withTimeout(0.25).andThen(new IndexerPercent().withTimeout(1.0))));
 
         aStart.and(aDPadUp).onTrue(new Reset0());
