a42e8050a1936e96826af0528f3cd2246fdad7af
Vihaan Rawat <31100481@students.sd308.org>
2026-03-12 19:38:12 -0500

    Added the isCompetetion boolean to elastic.

--- src/main/java/team/gif/robot/Robot.java
+++ src/main/java/team/gif/robot/Robot.java
@@ -53,7 +53,7 @@ public class Robot extends TimedRobot {
 
     private double delay;
     private final Timer delayTimer = new Timer();
-    public boolean isCompetition = false;
+    public static boolean isCompetition = false;
 
     private final CommandScheduler commandScheduler = CommandScheduler.getInstance();
     public static final boolean enableSwerveDebug = true;
