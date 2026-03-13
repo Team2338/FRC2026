097ebd99fe30a3595be0393ec54adf85deb52d8f
Caleb Herring <calebsethherring+github@gmail.com>
2026-03-12 21:48:53 -0500

    cleanup OI

--- src/main/java/team/gif/robot/commands/shooter/IndexerPercent.java
+++ src/main/java/team/gif/robot/commands/shooter/IndexerPercent.java
@@ -4,23 +4,24 @@ import edu.wpi.first.wpilibj2.command.Command;
 import team.gif.robot.Robot;
 
 public class IndexerPercent extends Command {
-    double bottomIndexerMotorPercent = 0;
-    double topIndexerMotorPercent = 0;
+    double bottomIndexerMotorPercent;
+    double topIndexerMotorPercent;
 
     /**
      * Runs both indexer motors at given percents
+     * @param topPercent percent to run top indexer (positive pulls fuel in)
+     * @param bottomPercent percent to run the bottom indexer (positive collects)
      */
-    public IndexerPercent() {
+    public IndexerPercent(double topPercent, double bottomPercent) {
         super();
         addRequirements(Robot.indexer);
+        bottomIndexerMotorPercent = bottomPercent;
+        topIndexerMotorPercent = topPercent;
     }
 
     // Called when the command is initially scheduled.
     @Override
-    public void initialize() {
-        bottomIndexerMotorPercent = 0.5;
-        topIndexerMotorPercent = 0.8;
-    }
+    public void initialize() {}
 
     // Called every time the scheduler runs (~20ms) while the command is scheduled
     @Override
