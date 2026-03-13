ff5904b7b5c0b47d5ba42f3be007d1eeed2e5193
Caleb Herring <calebsethherring+github@gmail.com>
2026-03-12 20:03:08 -0500

    WIP

--- src/main/java/team/gif/robot/subsystems/Agitator.java
+++ src/main/java/team/gif/robot/subsystems/Agitator.java
@@ -18,7 +18,7 @@ public class Agitator extends SubsystemBase {
     }
 
     public void setPercent(double percent) {
-        agitatorMotor.set(percent);
+        agitatorMotor.set(-percent);
     }
 
     public void stopMotor() {
