fce6cdb22162bbf5964986997476ae3d1f84d98b
Caleb Herring <calebsethherring+github@gmail.com>
2026-03-13 01:16:56 -0500

    Removed unused import

--- src/main/java/team/gif/robot/commands/shooter/ShooterRPM.java
+++ src/main/java/team/gif/robot/commands/shooter/ShooterRPM.java
@@ -1,6 +1,5 @@
 package team.gif.robot.commands.shooter;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import team.gif.robot.Robot;
 
