644ea798f057eac34c0fc8f8fc377b0d38416a49
Bill <Knopf>
2026-03-23 01:19:56 -0500

    Additional comments

--- src/main/java/team/gif/robot/commands/autos/AutonCenterShoot.java
+++ src/main/java/team/gif/robot/commands/autos/AutonCenterShoot.java
@@ -15,7 +15,8 @@ public class AutonCenterShoot extends ParallelDeadlineGroup {
      * This is the entire sequence to shoot in autos at the start of the match
      * from the Center shot. This sequence deploys the collector only part way in
      * order to keep the fuel close to the shooter. Also runs the collector to
-     * move the fuel initially stored in the collector to the shooter.
+     * move the fuel initially stored in the collector to the shooter. Does not run agitator
+     * wheels because there are only 8 fuel in the hopper and no need to run the agitator.
      */
     public AutonCenterShoot() {
         super(new WaitCommand(4.0)); // max amount of time to run
