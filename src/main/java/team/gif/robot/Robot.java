7bf2acfa744ca0e394cb8a4c9cc164c19cb46415
Caleb Herring <calebsethherring+github@gmail.com>
2026-03-30 17:14:09 -0500

    fix logging

--- src/main/java/team/gif/robot/Robot.java
+++ src/main/java/team/gif/robot/Robot.java
@@ -5,6 +5,8 @@
 package team.gif.robot;
 
 import edu.wpi.first.net.PortForwarder;
+import edu.wpi.first.util.datalog.StringLogEntry;
+import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
