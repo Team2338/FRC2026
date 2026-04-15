7cefa64b7eec0d0052429b0bec8b6cdfebbc2da8
Caleb Herring <calebsethherring+github@gmail.com>
2026-04-15 00:36:24 -0500

    Hold collector down in auto
    DefaultCommand to hold collector down was not getting scheduled. Moved functionality into AutonCollectorDown

--- src/main/java/team/gif/robot/commands/collector/collectorautos/CollectorAutonPivotDown.java
+++ src/main/java/team/gif/robot/commands/collector/collectorautos/CollectorAutonPivotDown.java
@@ -1,13 +1,14 @@
 package team.gif.robot.commands.collector.collectorautos;
 
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import team.gif.robot.Constants;
 import team.gif.robot.Robot;
 
 public class CollectorAutonPivotDown extends Command {
     /**
      * Moves the collector down at a very high rate of speed. Slows the speed down when close to
-     * being fully deployed. Re-enables the default command for holding the collector fully deployed.
+     * being fully deployed. Holds the collector down.
      * Used at the beginning of autonomous to deploy the collector fast.
      */
     public CollectorAutonPivotDown() {
@@ -22,18 +23,20 @@ public class CollectorAutonPivotDown extends Command {
     // Called every time the scheduler runs (~20ms) while the command is scheduled
     @Override
     public void execute() {
-        Robot.pivotMotor.runPivotPercent(1.0);
+        if (Robot.pivotMotor.getPosition() < Constants.Collector.PIVOT_DEPLOYED_ENCODER_POS * 0.9) {
+            Robot.pivotMotor.runPivotPercent(1.0);
+        } else {
+            Robot.pivotMotor.runPivotPercent(Constants.Collector.PIVOT_DOWN_HOLD_PERCENT);
+        }
     }
 
     // Return true when the command should end, false if it should continue. Runs every ~20ms.
     @Override
     public boolean isFinished() {
-        return Robot.pivotMotor.getPosition() > Constants.Collector.PIVOT_DEPLOYED_ENCODER_POS * 0.90;
+        return false;
     }
 
     // Called when the command ends or is interrupted.
     @Override
-    public void end(boolean interrupted) {
-        Robot.pivotMotor.setDefaultCommand(new CollectorAutoPivotHold());
-    }
+    public void end(boolean interrupted) {}
 }
