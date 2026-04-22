90a7ee94ed861842b732951d2fb3c7e2f79bce17
Accidental MIstake <18001870@students.sd308.org>
2026-04-21 20:12:44 -0500

    Auto StealPass finished, needs tuning

--- src/main/java/team/gif/robot/RobotContainer.java
+++ src/main/java/team/gif/robot/RobotContainer.java
@@ -48,7 +48,6 @@ public class RobotContainer {
         NamedCommands.registerCommand("DetectBeach", new DetectBeach());
         NamedCommands.registerCommand("AutonEject", new AutonEject());
 
-
 //        NamedCommands.registerCommand("CC-autonshoot", new AutonInitialShoot()); // no longer used since bot no longer shoots first. Would need additional testing
 
         // Configure the trigger bindings
@@ -78,6 +77,7 @@ public class RobotContainer {
         names.add("Right-FuelCross-HubNear");
         names.add("Right-NeutralMid-HubNear");
         names.add("Left-NeutralMid-HubNear");
+        names.add("Left-Fwd-StealPass");
 
         ArrayList<PathPlannerAuto> autoChoices = new ArrayList<>();
 
