8465ad3ee6e5a21b02db75b17f153f99350a2f9a
Bill <billknopfjr@yahoo.com>
2026-04-22 09:25:50 -0500

    Need to also run agitator

--- src/main/java/team/gif/robot/commands/autos/AutonStealShoot.java
+++ src/main/java/team/gif/robot/commands/autos/AutonStealShoot.java
@@ -1,7 +1,10 @@
 package team.gif.robot.commands.autos;
 
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
+import team.gif.robot.Constants;
+import team.gif.robot.Robot;
 import team.gif.robot.commands.collector.CollectorPercent;
 import team.gif.robot.commands.collector.collectorautos.CollectorAutoPivotDown;
 import team.gif.robot.commands.shooter.IndexerShoot;
@@ -20,7 +23,7 @@ public class AutonStealShoot extends ParallelDeadlineGroup {
         addCommands(
                 // can not use shooterAuto and HubAutoALign because the collector is in the way of the cameras
                 new ShooterRPM(3600),
-                new CollectorAutoPivotDown(0.5).alongWith(new CollectorPercent(0.8).withTimeout(0.5)).andThen(new IndexerShoot())
+                new CollectorAutoPivotDown(0.5).alongWith(new CollectorPercent(0.8).withTimeout(0.5)).andThen(new IndexerShoot().alongWith(new InstantCommand(()->Robot.agitator.setPercent(Constants.Collector.AGITATOR_MOTOR_PERCENT))))
         );
     }
 }
