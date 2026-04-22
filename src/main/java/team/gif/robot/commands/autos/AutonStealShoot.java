46b035a7fd43e18c69a7d5a4d7877e0f1f25e35b
Bill <billknopfjr@yahoo.com>
2026-04-22 09:33:25 -0500

    Break out single line shoot sequence

--- src/main/java/team/gif/robot/commands/autos/AutonStealShoot.java
+++ src/main/java/team/gif/robot/commands/autos/AutonStealShoot.java
@@ -2,6 +2,7 @@ package team.gif.robot.commands.autos;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import team.gif.robot.Constants;
 import team.gif.robot.Robot;
@@ -21,9 +22,21 @@ public class AutonStealShoot extends ParallelDeadlineGroup {
     public AutonStealShoot() {
         super(new WaitCommand(4.0)); // max amount of time to run
         addCommands(
-                // can not use shooterAuto and HubAutoALign because the collector is in the way of the cameras
-                new ShooterRPM(3600),
-                new CollectorAutoPivotDown(0.5).alongWith(new CollectorPercent(0.8).withTimeout(0.5)).andThen(new IndexerShoot().alongWith(new InstantCommand(()->Robot.agitator.setPercent(Constants.Collector.AGITATOR_MOTOR_PERCENT))))
+            // can not use shooterAuto and HubAutoALign because the collector is in the way of the cameras
+            new ShooterRPM(3600),
+            new SequentialCommandGroup(
+                new ParallelDeadlineGroup(
+                    new WaitCommand(0.5), // timeout
+                    new CollectorAutoPivotDown(0.5),
+                    new CollectorPercent(0.8)
+                ),
+                new ParallelDeadlineGroup(
+                    new IndexerShoot(),
+                    new InstantCommand(()->Robot.agitator.setPercent(Constants.Collector.AGITATOR_MOTOR_PERCENT)
+                    )
+                )
+            )
+//                new CollectorAutoPivotDown(0.5).alongWith(new CollectorPercent(0.8).withTimeout(0.5)).andThen(new IndexerShoot().alongWith(new InstantCommand(()->Robot.agitator.setPercent(Constants.Collector.AGITATOR_MOTOR_PERCENT))))
         );
     }
 }
